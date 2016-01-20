c7420d3fa8397634f317a28297bdab9d1d0bc8b1
Jeff Scott Brown <brownj@ociweb.com>
2016-01-19 20:33:30 -0600

    configure Travis to auto publish

--- travis-build.sh
+++ travis-build.sh
@@ -3,5 +3,16 @@ set -e
 ./gradlew clean check
 
 EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  echo "Publishing archives"
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      ./gradlew publish || EXIT_STATUS=$?
+  fi
+fi
 
 exit $EXIT_STATUS
